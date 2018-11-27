58635b3e7e0b27ea1e9b71c69fbcc6d0a37f77ec
caishu <caishu@polaristech.io>
2018-11-28 00:44:28 +0800

    add dummy eureka client with REST API at /service1/greeting

---
+++ src/main/java/io/polaristech/eurekaDemoClient/EurekaDemoClientApplication.java
@@ -0,0 +1,14 @@
+package io.polaristech.eurekaDemoClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaDemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDemoClientApplication.class, args);
+	}
+}
